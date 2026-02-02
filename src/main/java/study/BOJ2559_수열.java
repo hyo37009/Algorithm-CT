f60f87b0c9e2bec403de1785e8f92a4e7eb5ed38
hyo37 <hyo37009@gmail.com>
2026-02-03 07:01:54 +0900

    rename: [BOJ2559] 수열 파일명 수정 및 JavaDoc 추가
    - 파일명을 BJ2559_수열 → BOJ2559_수열로 변경하여 컨벤션 통일
    - JavaDoc 추가: 문제 정보, 접근 방법, 시간복잡도, 링크 등 포함
    - 슬라이딩 윈도우 기법으로 O(n)의 시간복잡도 유지

--- src/main/java/study/BOJ2559_수열.java
+++ src/main/java/study/BOJ2559_수열.java
@@ -1,10 +1,30 @@
 package study;
 
-import java.io.*;
 import java.util.*;
 import java.util.stream.IntStream;
 
-public class BJ2559_수열 {
+/**
+ * [백준] 2559 - 수열
+ *
+ * [문제 정보]
+ * - 난이도: 실버 3
+ * - 분류: 수열의 합
+ * - 링크: https://www.acmicpc.net/problem/2559
+ *
+ * [문제 요약]
+ * 길이 n인 배열의
+ *
+ * [접근 방법]
+ * 실패 - 매번 새 배열을 만듦 (메모리 초과)
+ * 성공 - 슬라이딩 윈도우
+ * [시간복잡도]
+ * 실패 -
+ * 성공 - O(n)
+ *
+ * [특이사항]
+ * Intstream, BufferedReader를 공부하고싶음
+ */
+public class BOJ2559_수열 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int[] days = Arrays.stream(scanner.nextLine().split(" "))
