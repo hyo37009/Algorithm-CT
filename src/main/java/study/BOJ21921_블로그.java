c65abb7019d03ab369ddc17342dd4700c0a3a916
hyo37009 <hyo37009@gmail.com>
2026-02-19 07:13:38 +0900

    failed: [BOJ2304] BOJ21921_블로그

--- src/main/java/study/BOJ21921_블로그.java
+++ src/main/java/study/BOJ21921_블로그.java
@@ -6,38 +6,37 @@ import java.util.*;
 public class BOJ21921_블로그 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         int[] input = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
+        int n = input[0];
         int x = input[1];
         input = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
         br.close();
 
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < input.length - x + 1; i++) {
-            int now = 0;
-            for(int j = 0; j < x; j++)
-                now += input[i + j];
-            max = Math.max(max, now);
-        }
-
-        int r = 0;
-        for (int i = 0; i < input.length - x + 1; i++) {
-            int now = 0;
-            for(int j = 0; j < x; j++)
-                now += input[i + j];
-            if (now == max) r++;
+        int max = 0;
+        int count = 1;
+        int now = 0;
+        for(int i = 0; i < x; i++)
+            now += input[i];
+        for (int i = x; i < input.length; i++) {
+            now = now - input[i - x] + input[i];
+            if (max < now){
+                max = now;
+                count = 1;
+            } else if (max == now) {
+                count++;
+            }
         }
         if (max == 0) {
             System.out.println("SAD");
             return;
         }
         System.out.println(max);
-        System.out.println(r);
+        System.out.println(count);
     }
 }
