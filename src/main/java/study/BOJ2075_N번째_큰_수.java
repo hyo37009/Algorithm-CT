b7a22329a21a9d7fe8f7fa2567c44950cdb1d389
hyo37009 <hyo37009@gmail.com>
2026-02-27 08:14:28 +0900

    fail: [BOJ2075] N번째 큰 수 메모리 초과

--- src/main/java/study/BOJ2075_N번째_큰_수.java
+++ src/main/java/study/BOJ2075_N번째_큰_수.java
@@ -9,50 +9,19 @@ public class BOJ2075_N번째_큰_수 {
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
-        int[][] nums = new int[n][n];
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i = 0; i < n; i++) {
             String[] ns = br.readLine().split(" ");
             for (int j = 0; j < n; j++) {
-                nums[i][j] = Integer.parseInt(ns[j]);
+                pq.offer(Integer.parseInt(ns[j]));
+                if(pq.size() > n)
+                    pq.poll();
             }
         }
         br.close();
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        for (int i = 0; i < n; i++) {
-            pq.offer(new Node(nums[i][n - 1], i, n - 1));
-        }
-
-        int count = 0;
-        int answer = 0;
-        while (count < n) {
-            Node node = pq.poll();
-            answer = node.value;
-            pq.offer(new Node(nums[node.col][node.row - 1], node.col, node.row - 1));
-            count++;
-        }
-
-        System.out.println(answer);
-    }
-
-    static class Node implements Comparable<Node> {
-        int value;
-        int col;
-        int row;
-
-        public Node(int value, int col, int row) {
-            this.value = value;
-            this.col = col;
-            this.row = row;
-        }
-
-
-        @Override
-        public int compareTo(Node o) {
-            // 내림차순 정렬
-            return o.value - value;
-        }
+        System.out.println(pq.peek());
     }
 
 }
