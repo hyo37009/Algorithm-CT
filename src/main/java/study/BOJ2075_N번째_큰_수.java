f94501545217d8593e7f992df3aeb23cdbb902ff
hyo37009 <hyo37009@gmail.com>
2026-02-27 08:16:09 +0900

    fail: [BOJ2075] N번째 큰 수 메모리 초과

--- src/main/java/study/BOJ2075_N번째_큰_수.java
+++ src/main/java/study/BOJ2075_N번째_큰_수.java
@@ -6,15 +6,16 @@ import java.util.*;
 public class BOJ2075_N번째_큰_수 {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
 
     public static void main(String[] args) throws IOException {
         int n = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for (int i = 0; i < n; i++) {
-            String[] ns = br.readLine().split(" ");
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                pq.offer(Integer.parseInt(ns[j]));
+                pq.offer(Integer.parseInt(st.nextToken()));
                 if(pq.size() > n)
                     pq.poll();
             }
