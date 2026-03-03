0963770d85747da1f952c6e49ba9a9e21865c0d5
hyo37009 <hyo37009@gmail.com>
2026-03-04 08:34:23 +0900

    solve: [BOJ2531] 회전 초밥 문제 해결
    - 슬라이딩 윈도우와 배열 기반 초밥 개수 추적으로 최대 가짓수 계산
    - HashSet 사용을 배열로 교체해 성능 개선
    - JavaDoc 주석 HTML 태그 수정 및 정보 추가

--- src/main/java/study/BOJ2531_회전_초밥.java
+++ src/main/java/study/BOJ2531_회전_초밥.java
@@ -5,23 +5,23 @@ import java.util.*;
 
 /**
  * [백준] 2531 - 회전 초밥
- *
+ * <p>
  * [문제 정보]
  * - 난이도: 실버 1
  * - 분류:
  * - 링크: https://www.acmicpc.net/problem/2531
- *
+ * <p>
  * [문제 요약]
  * 연속 k개의 접시를 먹은 경우 할인도니 가격으로 제공
  * 위를 만족한 경우 쿠폰에 쓰인 초밥은 무료 (없다면 만들어줌)
  * 손님이 먹을 수 있는 초밥의 가짓수의 최댓값
- *
+ * <p>
  * [접근 방법]
- *
- *
+ * <p>
+ * <p>
  * [시간복잡도]
- *
- *
+ * <p>
+ * <p>
  * [특이사항]
  *
  */
@@ -44,24 +44,28 @@ public class BOJ2531_회전_초밥 {
         }
         br.close();
 
-        Set<Integer> eat = new HashSet<>();
-        eat.add(c);
+        int[] freq = new int[d + 1];
+        freq[c]++;
+
+        int answer = 1;
         for (int i = 0; i < k; i++) {
-            // 초기 세팅
-            eat.add(sushi[i]);
+            if (freq[i] == 0) {
+                answer++;
+            }
+            freq[i]++;
         }
 
-        int answer = eat.size();
-        for (int i = 0; i < n; i++) {
-            int last = (i + k - 1 < n)? i + k - 1 : i + k - 1 - n; // 새 접시 인덱스
-            // 첫 접시 뺌
-            eat.remove(sushi[Math.max(i - 1, 0)]);
-            eat.add(sushi[last]); // 새 접시 추가
-            eat.add(c); // 쿠폰 추가
+        for (int i = 0; i < k; i++) {
+            int oldSushi = i;
+            int newSushi = i + k % n;
 
-            answer = Math.max(answer, eat.size());
+            if(freq[oldSushi]-- == 0)
+                answer--;
+            else if (freq[newSushi]++ == 1)
+                answer++;
         }
 
         System.out.println(answer);
     }
+
 }
